f734f14568f95272467fd5bcdf22fd7be943639e
marianafs15 <marianahueck@gmail.com>
2024-06-19 21:36:08 +0000

    cree los arrays, cree el parrafo de la excusa y lo proximo es la funcion para obtener el numero aleatoreo

--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = document.getElementById("excuse");
+  let who = ["Mi gato", "El vecino", "Mi hermano", "El policía"];
+  let action = ["mordió", "tiró", "manchó", "perdió"];
+  let what = ["mis zapatos", "el libro", "la computadora", "las llaves"];
+  let when = [
+    "en la mañana",
+    "durante la fiesta",
+    "mientras cocinaba",
+    "al anochecer",
+    "cuando llegué a casa"
+  ];
+  const getRandomInt = array => {
+    return;
+  };
+  excuse.innerHTML = who[2] + " " + action[2] + " " + what[0] + " " + when[3];
 };
